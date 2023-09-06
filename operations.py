514403072e433cea782a1660829b16b9277c6b05
Asim Mehmood <asim@tintash.com>
2023-09-06 13:17:31 +0500

    fix: added operaiton file

---
+++ operations.py
@@ -0,0 +1,20 @@
+"""arithmetic operations functions"""
+
+def add(op1, op2):
+    """Add given params"""
+    return op1 + op2
+
+
+def minus(op1, op2):
+    """Subtract given params"""
+    return op1 - op2
+
+
+def multiply(op1, op2):
+    """Multiply given params"""
+    return op1 * op2
+
+
+def divide(op1, op2):
+    """divide given params"""
+    return op1 / op2
