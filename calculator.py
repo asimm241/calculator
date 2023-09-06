144b99408f702d8d4f05e3a1a7b6cfc805f8f4f6
Asim Mehmood <asim@tintash.com>
2023-09-06 13:15:39 +0500

    format code added exception handling

--- calculator.py
+++ calculator.py
@@ -1,38 +1,26 @@
 """calculator for four simple operation"""
-
-def add(op1, op2):
-    """Add given params"""
-    return op1 + op2
-
-
-def subtract(op1, op2):
-    """Subtract given params"""
-    return op1 - op2
-
-
-def multiply(op1, op2):
-    """Multiply given params"""
-    return op1 * op2
-
-
-def divide(op1, op2):
-    """divide given params"""
-    return op1 / op2
-
+import operations as math
 
 exitCommand: str = ''
 while exitCommand != 'exit':
-    operation = input(" + \n - \n * \n /")
-    num1 = int(input("operand: "))
-    num2 = int(input("operand: "))
+    operation = input("Enter operation\n + \n - \n * \n /\n")
+    try:
+        num1 = int(input("left operand: "))
+        num2 = int(input("right operand: "))
+    except ValueError:
+        print("invalid operands only digits allowed")
+        continue
 
     if operation == "+":
-        print(add(num1, num2))
-    if operation == "-":
-        print(subtract(num1, num2))
-    if operation == "*":
-        print(multiply(num1, num2))
-    if operation == "/":
-        print(divide(num1, num2))
+        print(f"{num1} + {num2} = {math.add(num1, num2)}")
+    elif operation == "-":
+        print(f"{num1} - {num2} = {math.minus(num1, num2)}")
+    elif operation == "*":
+        print(f"{num1} * {num2} = {math.multiply(num1, num2)}")
+    elif operation == "/":
+        print(f"{num1} / {num2} = {math.divide(num1, num2)}")
+    else:
+        print(f"can not perform \"{operation}\"")
 
-    exitCommand = input()
+    exitCommand = input(
+        "enter \"exit\" to exit or anything else to continue\n")
